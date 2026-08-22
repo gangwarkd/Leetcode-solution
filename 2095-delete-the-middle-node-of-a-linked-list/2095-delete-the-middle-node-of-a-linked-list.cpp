226aef0b4329ed68c251afbecf884401a6e73c56
Kuldeep Gangwar <gangwarkuldeep9012@gmail.com>
2026-08-22 23:30:20 +0530

    Time: 4 ms (34.75%), Space: 312 MB (56.72%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,34 +11,34 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-    //     ListNode* slow=head;
-    //     ListNode* fast=head;
-    //     if(fast->next==NULL) return NULL;
-    //     while(fast->next->next!=NULL && fast->next->next->next!=NULL){
-    //         slow=slow->next;
-    //         fast=fast->next->next;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        if(fast->next==NULL) return NULL;
+        while(fast->next->next!=NULL && fast->next->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
 
-    //     }
-    //     slow->next=slow->next->next;
-    //     return head;
- 
-     if (head == NULL || head->next == NULL)
-            return NULL;
-   int len=0;
-        ListNode* temp=head;
-
-        while(temp!=NULL){
-            len++;
-            temp=temp->next;
         }
-        int mididx=len/2;
-        ListNode* mid=head;
-        for(int i=1;i<mididx;i++){
-            mid=mid->next;
-        
-        }
-        mid->next=mid->next->next;
+        slow->next=slow->next->next;
         return head;
+ 
+//      if (head == NULL || head->next == NULL)
+//             return NULL;
+//    int len=0;
+//         ListNode* temp=head;
+
+//         while(temp!=NULL){
+//             len++;
+//             temp=temp->next;
+//         }
+//         int mididx=len/2;
+//         ListNode* mid=head;
+//         for(int i=1;i<mididx;i++){
+//             mid=mid->next;
+        
+//         }
+//         mid->next=mid->next->next;
+//         return head;
 
     }
 };
