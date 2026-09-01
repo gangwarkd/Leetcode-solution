71fce61c0bfd2c352553890eb148c3d891b7b5fa
Kuldeep Gangwar <gangwarkuldeep9012@gmail.com>
2026-09-01 22:43:19 +0530

    Time: 1 ms (26.83%), Space: 8.5 MB (5.66%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,35 @@
+
+        class Solution {
+public:
+    int sumOfSquares(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+    bool isHappy(int n) {
+
+        unordered_set<int> seen;
+
+        while (n != 1) {
+
+            // If we have seen this number before,
+            // we are stuck in a cycle.
+            if (seen.count(n))
+                return false;
+
+            seen.insert(n);
+
+            n = sumOfSquares(n);
+        }
+
+        return true;
+    
+    }
+};
