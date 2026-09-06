cf1c798ccc9197387bddeb01f5e7ad6e14da8c22
Kuldeep Gangwar <gangwarkuldeep9012@gmail.com>
2026-09-06 23:25:32 +0530

    Time: 0 ms (100%), Space: 263.4 MB (14.01%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,19 @@
+
+        class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        k = k % n;
+
+        // Reverse the whole array
+        reverse(nums.begin(), nums.end());
+
+        // Reverse first k elements
+        reverse(nums.begin(), nums.begin() + k);
+
+        // Reverse remaining elements
+        reverse(nums.begin() + k, nums.end());
+    
+    }
+};
